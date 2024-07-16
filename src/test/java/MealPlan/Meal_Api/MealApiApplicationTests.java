8514a54a4c6325ba91ff23eb46d2349e164c476e
Kpotter9 <kjpu80129@gmail.com>
2024-07-16 00:56:17 -0400

    Initial commit

---
+++ src/test/java/MealPlan/Meal_Api/MealApiApplicationTests.java
@@ -0,0 +1,13 @@
+package MealPlan.Meal_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
